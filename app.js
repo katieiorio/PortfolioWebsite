95a5c86073e84b93ef42c87fdef6499d26d78b00
katieiorio <katiorio1999@gmail.com>
2024-04-04 13:58:46 -0400

    Created White Overlay over Images
    4/4

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ function imageTitleHover() {
     let length = imageHovers.length;
     for (i; i<length; i++) {
         if (document.addEventListener) {
-            imageHovers[i].addEventListener("mouseover", function() {
-                imageHovers[i].firstElementChild.display = block;
-            console.log("yoyoyo")
-            });
-        } else {
-            imageHovers[i].attachEvent("onclick", function() {
-                // use buttons[i] to target clicked button
-            });
+            imageHovers[i].addEventListener("mouseover", function(index) {
+                return function() {
+                    imageHovers[index].firstElementChild.style.display = 'flex';
+                    console.log("yoyoyo");
+                };
+            }(i));
+        } 
+        if (document.addEventListener) {
+            imageHovers[i].addEventListener("mouseout", function(index) {
+                return function() {
+                    imageHovers[index].firstElementChild.style.display = 'none';
+                    console.log("yoyoyo");
+                };
+            }(i));
         };
     };
 };
+
+imageTitleHover();
+
+// console.log("joyoyo");
