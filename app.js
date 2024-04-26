08d1d4abc3e41ada52c3b0e03d5b9f334911cfc7
katieiorio <katiorio1999@gmail.com>
2024-04-25 23:57:52 -0400

    AddedGifs to Home 4/25

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ function imageTitleHover() {
             imageHovers[i].addEventListener("mouseover", function (index) {
                 return function () {
                     imageHovers[index].firstElementChild.style.display = 'flex';
-                    console.log("yoyoyo");
                 };
             }(i));
         }
@@ -15,7 +14,6 @@ function imageTitleHover() {
             imageHovers[i].addEventListener("mouseout", function (index) {
                 return function () {
                     imageHovers[index].firstElementChild.style.display = 'none';
-                    console.log("yoyoyo");
                 };
             }(i));
         };
@@ -24,27 +22,100 @@ function imageTitleHover() {
 
 imageTitleHover();
 
-function amazonScroll() {
-    const scrollArray = [1];
-    window.addEventListener('scroll', function () {
-        scrollArray.push(window.scrollY);
-        const lastIndex = scrollArray.length - 1;
-        if (scrollArray[lastIndex] >= 998) {
-            const selectAmazonAplusText = document.getElementById("amazonAplusText");
-            const selectAmazonAltText = document.getElementById("amazonAltText");
-            selectAmazonAplusText.style.display = 'none';
-            selectAmazonAltText.style.display = 'flex';
-            console.log("yoyoyo");
+function homeLinks() {
+    const webDiv = document.getElementById("homeImageLink1");
+    const motionDiv = document.getElementById("homeImageLink2");
+    const brandingDiv = document.getElementById("homeImageLink3");
+    const amazonDiv = document.getElementById("homeImageLink4");
+        if (webDiv) {
+            webDiv.addEventListener("click", function() {
+                window.location.href = "development.html";
+            });
+        } 
+        if (motionDiv) {
+            motionDiv.addEventListener("click", function() {
+                window.location.href = "motion.html";
+            });
         }
-        else {
-            const selectAmazonAplusText = document.getElementById("amazonAplusText");
-            const selectAmazonAltText = document.getElementById("amazonAltText");
-            selectAmazonAplusText.style.display = 'flex';
-            selectAmazonAltText.style.display = 'none';
-            console.log("yoyoyo");
-            console.log(window.scrollY);
+        if (brandingDiv) {
+            brandingDiv.addEventListener("click", function() {
+                window.location.href = "branding.html";
+            });
         }
-    });
+        if (amazonDiv) {
+            amazonDiv.addEventListener("click", function() {
+                window.location.href = "amazon.html";
+            });
+        }
 }
+homeLinks();
 
-amazonScroll();
+
+
+
+
+
+
+
+
+
+
+//katie's web development motto
+
+if (hardwork === true) {
+    success();
+} else {null}
+
+
+
+
+
+
+
+
+
+
+const webDiv = document.getElementById("homeImageLink1");
+const motionDiv = document.getElementById("homeImageLink2");
+const brandingDiv = document.getElementById("homeImageLink3");
+const amazonDiv = document.getElementById("homeImageLink4");
+
+    webDiv.addEventListener('click', function() {
+        window.location.href = "development.html";
+    })
+    motionDiv.addEventListener('click', function() {
+        window.location.href = "motion.html";
+    })
+    brandingDiv.addEventListener('click', function() {
+        window.location.href = "branding.html";
+    })
+    amazonDiv.addEventListener('click', function() {
+        window.location.href = "amazon.html";
+    })
+
+
+    // function amazonScroll() {
+    //     const scrollArray = [1];
+    //     window.addEventListener('scroll', function () {
+    //         scrollArray.push(window.scrollY);
+    //         const lastIndex = scrollArray.length - 1;
+    //         const rect = document.getElementById("amazonAlt").getBoundingClientRect();
+    //         if (scrollArray[lastIndex] >= (rect.top + window.scrollY)) {
+    //             const selectAmazonAplusText = document.getElementById("amazonAplusText");
+    //             const selectAmazonAltText = document.getElementById("amazonAltText");
+    //             selectAmazonAplusText.style.display = 'none';
+    //             selectAmazonAltText.style.display = 'flex';
+    //             console.log("yoyoyo");
+    //         }
+    //         else {
+    //             const selectAmazonAplusText = document.getElementById("amazonAplusText");
+    //             const selectAmazonAltText = document.getElementById("amazonAltText");
+    //             selectAmazonAplusText.style.display = 'flex';
+    //             selectAmazonAltText.style.display = 'none';
+    //             console.log("yoyoyo");
+    //             console.log(window.scrollY);
+    //         }
+    //     });
+    // }
+    
+    // amazonScroll();
